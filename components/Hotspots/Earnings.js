0e7a95320101a6ded1babfafff08c43af015395d
angelo-marasa <angelo.marasa@kaleidico.com>
2022-02-15 11:22:47 -0500

    Pinging heroku every minute

--- components/Hotspots/Earnings.js
+++ components/Hotspots/Earnings.js
@@ -156,6 +156,10 @@ const Earnings = ({address, hotspotID, delay, editHandler, edit, apiDomain, heli
                         handleCurrentPrice(res.data.data['price']);
                          setTimeout(function() {
                             handleLoading();
+                            const thorPrice = await axios.get(`https://thor-discordpricingbot.herokuapp.com/`);
+                            const heliumPrice = await axios.get(`https://helium-discordpricebot.herokuapp.com/`);
+                            const planetsPrice = await axios.get(`https://planets-discordpricebot.herokuapp.com/`);
+
                         }, delay * 1000);
                     })
                 
